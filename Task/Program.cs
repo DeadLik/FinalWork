57b5a7e12ea1a795d2d46ef4cac98275eb2b0791
Игорь <mc_grove@bk.ru>
2022-12-21 11:04:35 -0500

    add method number 1

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ArrayLength(string[] array)
+{
+    int lenghtArr = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) lenghtArr++;
+    }
+
+    return lenghtArr;
+}
